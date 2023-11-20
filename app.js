3076479f4eea1478e258a07628c688bd0ba3b683
spider05cyber <jaydenscarpaspam@gmail.com>
2023-11-20 14:37:27 +0000

    implementation of error handling

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-const express = require('express')
-const path = require('path')
-const stocks = require('./stocks')
+const express = require('express');
+const path = require('path');
+const stocks = require('./stocks');
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+const app = express();
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
-})
+  try {
+    const stockSymbols = await stocks.getStocks();
+    res.send({ stockSymbols });
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
-})
+  try {
+    const { params: { symbol } } = req;
+    const data = await stocks.getStockPoints(symbol, new Date());
+    res.send(data);
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-app.listen(3000, () => console.log('Server is running!'))
+app.listen(3000, () => console.log('Server is running!'));
--- static/scripts.js
+++ static/scripts.js
@@ -1,24 +1,69 @@
-const canvas = document.getElementById('chart')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById('chart');
+const ctx = canvas.getContext('2d');
 
-function drawLine (start, end, style) {
-  ctx.beginPath()
-  ctx.strokeStyle = style || 'black'
-  ctx.moveTo(...start)
-  ctx.lineTo(...end)
-  ctx.stroke()
+function drawLine(start, end, style) {
+  ctx.beginPath();
+  ctx.strokeStyle = style || 'black';
+  ctx.moveTo(...start);
+  ctx.lineTo(...end);
+  ctx.stroke();
 }
 
-function drawTriangle (apex1, apex2, apex3) {
-  ctx.beginPath()
-  ctx.moveTo(...apex1)
-  ctx.lineTo(...apex2)
-  ctx.lineTo(...apex3)
-  ctx.fill()
+function drawTriangle(apex1, apex2, apex3) {
+  ctx.beginPath();
+  ctx.moveTo(...apex1);
+  ctx.lineTo(...apex2);
+  ctx.lineTo(...apex3);
+  ctx.fill();
 }
 
-drawLine([50, 50], [50, 550])
-drawTriangle([35, 50], [65, 50], [50, 35])
+drawLine([50, 50], [50, 550]); // y-axis top coord, bottom coord
+drawTriangle([35, 50], [65, 50], [50, 35]);
 
-drawLine([50, 550], [950, 550])
-drawTriangle([950, 535], [950, 565], [965, 550])
+drawLine([50, 550], [950, 550]); // x-axis left coord, right coord
+drawTriangle([950, 535], [950, 565], [965, 550]);
+
+async function fetchData() {
+  const spinner = document.getElementById('loadingSpinner');
+  try {
+    spinner.classList.add('spin');
+
+    const response = await fetch('/stocks');
+    
+    if (!response.ok) {
+      throw new Error(`${response.status} - ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    const stockSymbols = data.stockSymbols;
+
+    console.log(stockSymbols);
+
+    stockSymbols.forEach(async symbol => {
+      try {
+        const stockDataResponse = await fetch(`/stocks/${symbol}`);
+        
+        if (!stockDataResponse.ok) {
+          throw new Error(`${stockDataResponse.status} - ${stockDataResponse.statusText}`);
+        }
+
+        const stockData = await stockDataResponse.json();
+        console.log(stockData);
+      } catch (error) {
+        alert(`Error fetching individual stock data for ${symbol}: ${error}`);
+        console.error(`Error fetching individual stock data for ${symbol}`, error);
+      }
+    });
+
+    spinner.classList.remove('spin');
+    spinner.hidden = true;
+  } catch (error) {
+    alert('Error fetching stock symbols list:\n' + error.message);
+    console.error('Error fetching stock symbols list:', error);
+    spinner.classList.remove('spin');
+    spinner.hidden = true;
+  }
+}
+
+// Call the fetchData function
+fetchData();
